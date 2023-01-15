0fb755f56a56e54ad6fa90203c8080559a6622bd
flisoch <flisochcs@gmail.com>
2023-01-15 18:42:35 +0300

    loading simple external program with grub modules

--- src/kernel/kmain.c
+++ src/kernel/kmain.c
@@ -2,6 +2,7 @@
 #include "interrupts.h"
 #include "serial.h"
 #include "gdt.h"
+#include "multiboot.h"
 
 int sum_of_three(int arg1, int arg2, int arg3) { return arg1 + arg2 + arg3; }
 
@@ -11,9 +12,33 @@ void init() {
     install_idt();
 }
 
-void kmain() {
+void print_hello_world() {
+    char message[] = "Life's good\n";
+    serial_write(COM1, message, sizeof(message ) - 1);
+}
+
+void kmain(unsigned int ebx) {
     init();
-    char buf[] = " Life's good ";
-    fb_write(buf, 14);
-    serial_write(COM1, buf, 13);
+    print_hello_world();
+
+    multiboot_info_t *mbinfo = (multiboot_info_t *)(long)ebx;
+    multiboot_module_t* modules = (multiboot_module_t*)(long)mbinfo->mods_addr;
+    unsigned int mod_addr = modules->mod_start;
+
+    if ((mbinfo->mods_count) == 1) {
+        char message[] = "ONE module loaded successfully";
+        serial_write(COM1, message, sizeof(message)-1);
+
+        typedef void(*call_module_t)(void);
+        call_module_t start_program = (call_module_t)(long) mod_addr;
+        start_program();
+    }
+    else if (mbinfo->mods_count == 0) {
+        char message[] = "Error: No modules loaded";
+        serial_write(COM1, message, sizeof(message)-1);
+    }
+    else {
+        char message[] = "Error: More than One module loaded";
+        serial_write(COM1, message, sizeof(message)-1);
+    }
 }
